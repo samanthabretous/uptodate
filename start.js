e65a18a7ec2a33b79f5161aba8f4751c49c8d752
Joshua Fermin <Joshuaf91@gmail.com>
2017-01-31 15:26:12 -0500

    setup eslint

--- start.js
+++ start.js
@@ -5,9 +5,9 @@ const environmentVariables = require('./server/env');
 sequelizeConnection.sequelize
 .authenticate()
 .then(() => {
-  //this if statement will prevent our express server and test server (using supertest) from trying to access the same port at the same time
+// this if statement will prevent our express server and test server (using supertest) from trying to access the same port at the same time
   if (!module.parent) {
-     app.listen(environmentVariables.PORT, () => console.log('Listening on port ' + environmentVariables.PORT));
-  };
+    app.listen(environmentVariables.PORT, () => console.log(`Listening on port ${environmentVariables.PORT}`));
+  }
 })
-.catch((err) => console.log('Unable to connect to the database:', err));
+.catch(err => console.log('Unable to connect to the database:', err));
