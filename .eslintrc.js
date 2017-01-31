3b51f09eaea7dec71b838573ca6982aee9f28b0d
Joshua Fermin <Joshuaf91@gmail.com>
2017-01-31 16:48:34 -0500

    follow airbnb lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+  "extends": "airbnb",
+  "plugins": [
+      "react",
+      "jsx-a11y",
+      "import"
+  ],
+  "globals": {
+    "document": true,
+    "foo": true,
+    "window": true
+  }
 };
--- server/db/models/class.js
+++ server/db/models/class.js
@@ -1,34 +1,34 @@
-'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var Class = sequelize.define('class', {
+
+module.exports = (sequelize, DataTypes) => {
+  const Class = sequelize.define('class', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
     description: {
       type: DataTypes.TEXT,
-      allowNull: false
+      allowNull: false,
     },
     schedule: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     location: {
       type: DataTypes.STRING,
-      allowNull: true
+      allowNull: true,
     },
     enrollmentCode: {
       type: DataTypes.STRING,
-      allowNull:false
-    }
+      allowNull: false,
+    },
   }, {
     classMethods: {
-      associate: function(models) {
-        Class.belongsToMany(models.user, {through: 'user_class'});
+      associate(models) {
+        Class.belongsToMany(models.user, { through: 'user_class' });
         Class.hasMany(models.lesson);
         Class.hasMany(models.assignment);
-      }
-    }
+      },
+    },
   });
   return Class;
 };
