dd7bcfdf8f58d46659a3818f6ec37b578149a12e
Joshua Fermin <Joshuaf91@gmail.com>
2017-02-05 15:36:11 -0500

    add ability to make directory

--- client/electron_view/containers/fileWatcher.js
+++ client/electron_view/containers/fileWatcher.js
@@ -7,17 +7,33 @@ export default (pathWatch) => {
   });
 
   watcher
-  .on('raw', (event, path, details) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      // this will be used in the router for the public folder.
+  .on('addDir', (path) => {
+    console.log('addDir path:', path);
+    console.log(path !== pathWatch);
+    // to prevent ajax call on root directory watch
+    if(path !== pathWatch) {
       let startingDirectorForPublic = pathWatch.split('/');
       startingDirectorForPublic = startingDirectorForPublic[startingDirectorForPublic.length - 1];
-      axios.post('http://localhost:2020/api/publicFile', {
+      axios.post('http://localhost:2020/api/publicFile/addDir', {
         publicPath: startingDirectorForPublic,
         localPath: path,
-        data,
       });
-      console.log('details', details);
-    });
+    }
+  })
+  .on('raw', (event, path, details) => {
+    console.log('event:', event);
+    // this if ensures axios call will only be sent if a file is modified
+    if (path.indexOf('.') !== -1 && event === 'change') {
+      fs.readFile(path, 'utf8', (err, data) => {
+        // this will be used in the router for the public folder.
+        let startingDirectorForPublic = pathWatch.split('/');
+        startingDirectorForPublic = startingDirectorForPublic[startingDirectorForPublic.length - 1];
+        axios.post('http://localhost:2020/api/publicFile', {
+          publicPath: startingDirectorForPublic,
+          localPath: path,
+          data,
+        });
+      });
+    }
   });
 };
