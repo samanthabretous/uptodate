b7eaafc0b5afd986a3eb28d369ccecc3bcf6b6ad
Gabemb <gabrielbny@gmail.com>
2017-02-06 12:04:38 -0500

    add lesson middleware functions

---
+++ server/web/routes/lesson/lesson_middleware.js
@@ -0,0 +1,48 @@
+const Lesson = require('../../db/models/index').lesson;
+
+// /api/lessons/new-lesson -- create new row in lesson table
+const createNewLesson = (req, res) => {
+  Lesson.create(req.body)
+    .then((newLesson) => {
+      res.send(newLesson);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const fetchLesson = (req, res) => {
+  Lesson.findById(req.params.lessonId, {
+    include: [{ all: true }],
+  })
+  .then((lesson) => {
+    res.send(lesson);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.sendStatus(500);
+  });
+};
+
+const updateLesson = (req, res) => {
+  Lesson.update({
+    name: req.body.name,
+    lecture: req.body.lecture,
+  }, {
+    where: { id: req.params.id },
+  })
+  .then((updatedLesson) => {
+    res.send(updatedLesson);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.sendStatus(500);
+  });
+};
+
+module.exports = {
+  createNewLesson,
+  fetchLesson,
+  updateLesson,
+};
