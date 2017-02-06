837e5d4b2f7447a552e82e4a4b780f24e5c86dc6
Gabemb <gabrielbny@gmail.com>
2017-02-05 20:18:19 -0500

    add lesson router

--- server/web/routes/index.js
+++ server/web/routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 router.use('/users', require('./user/user_router'));
 router.use('/classes', require('./class/class_router'));
+router.use('/lessons', require('./lesson/lesson_router'));
 
 
 module.exports = router;
