22af8f76ee4e87e59f042203f6cd77ffccaec51f
Gabemb <gabrielbny@gmail.com>
2017-02-12 16:00:02 -0500

    remove enrollmentCode

--- server/web/seed/class_seed.js
+++ server/web/seed/class_seed.js
@@ -4,25 +4,21 @@ module.exports = [
     description: 'Chemistry is Sodium fun!',
     schedule: 'Tuesdays, 3pm',
     location: 'Room 110B',
-    enrollmentCode: '1e2f3',
   }, {
     name: 'Biology',
     description: 'I find this humerus.',
     schedule: 'Wednesdays, 3pm',
     location: 'Room 510',
-    enrollmentCode: '4d5f6',
   }, {
     name: 'Physics',
     description: 'Physics is phun!',
     schedule: 'Thursdays, 3pm',
     location: 'Room 510',
-    enrollmentCode: '7c8v9',
   }, {
     name: 'Math',
     description: 'Don\'t be late for taking the rhombus!',
     schedule: 'Thursdays, 3pm',
     location: 'Room 510',
-    enrollmentCode: '1g0l1',
   },
 ];
 
