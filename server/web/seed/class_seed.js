c89e71fc45e7271a2b60db0101bf2f99ad5c815a
Gabemb <gabrielbny@gmail.com>
2017-02-12 01:46:34 -0500

    remove ownerId dummy data

--- server/web/seed/class_seed.js
+++ server/web/seed/class_seed.js
@@ -5,28 +5,24 @@ module.exports = [
     schedule: 'Tuesdays, 3pm',
     location: 'Room 110B',
     enrollmentCode: '1e2f3',
-    ownerId: 1,
   }, {
     name: 'Biology',
     description: 'I find this humerus.',
     schedule: 'Wednesdays, 3pm',
     location: 'Room 510',
     enrollmentCode: '4d5f6',
-    ownerId: 1,
   }, {
     name: 'Physics',
     description: 'Physics is phun!',
     schedule: 'Thursdays, 3pm',
     location: 'Room 510',
     enrollmentCode: '7c8v9',
-    ownerId: 1,
   }, {
     name: 'Math',
     description: 'Don\'t be late for taking the rhombus!',
     schedule: 'Thursdays, 3pm',
     location: 'Room 510',
     enrollmentCode: '1g0l1',
-    ownerId: 1,
   },
 ];
 
