d44f84adfc23e32056ef0591b7dcf8f7391b03ae
Melanie Williams <melaniep518@gmail.com>
2017-02-13 03:44:30 -0500

    add suggested changes

--- client/web_view/containers/StudentTeacherModal/StudentTeacherModal.jsx
+++ client/web_view/containers/StudentTeacherModal/StudentTeacherModal.jsx
@@ -5,6 +5,7 @@ import { StudentOrTeacher } from '../index';
 
 const mapStateToProps = state => ({
   isOpen: state.login.openStudentTeacherModal,
+  username: state.login.username,
 });
 
 const StudentTeacherModal = props => (
@@ -12,12 +13,18 @@ const StudentTeacherModal = props => (
     isOpen={props.isOpen}
     contentLabel="Student or Teacher Modal"
   >
+    <h2>{`Welcome, ${props.username}`}</h2>
     <StudentOrTeacher />
   </Modal>
 );
 
 StudentTeacherModal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
+  username: PropTypes.string,
+};
+
+StudentTeacherModal.defaultProps = {
+  username: '',
 };
 
 export default connect(mapStateToProps)(StudentTeacherModal);
