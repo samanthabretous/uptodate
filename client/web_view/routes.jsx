7c50ae8322755f7513847d86dbf0e4fa3e2a16bb
Gabemb <gabrielbny@gmail.com>
2017-02-17 04:32:04 -0500

    remove extra files

--- client/web_view/routes.jsx
+++ client/web_view/routes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import { App, Modal, LoginOrSignUp, HomePage, StudentOrTeacher, Dashboard, Lesson, DisplayClassLessons, ViewInstructorCode, AddAssignment, AddLesson } from './containers';
 import { getTitlebarInfo } from '../redux/titlebar';
-import { enterGetLessons, AsyncGetLessons } from '../redux/lesson';
+import { enterGetLessons, enterFetchLessons } from '../redux/lesson';
 
 export default (
   <Route component={App} >
@@ -14,7 +14,7 @@ export default (
     </Route>
     <Route path="/instructorcode" component={ViewInstructorCode} />
     <Route path="dashboard/:user/:currentClassCode" onEnter={getTitlebarInfo} component={Dashboard}>
-      <IndexRoute component={DisplayClassLessons} onEnter={enterGetLessons} />
+      <IndexRoute component={DisplayClassLessons} onEnter={enterFetchLessons} />
       <Route path="lesson/:classId" component={AddLesson} />
       <Route path=":lesson" component={Lesson} />
       <Route path="assignment/:classId" component={AddAssignment} onEnter={enterGetLessons} />
