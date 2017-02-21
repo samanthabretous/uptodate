f3989d39553786c6ea40ebb2288f4fb903facd8e
Gabemb <gabrielbny@gmail.com>
2017-02-20 21:28:37 -0500

    all dummy users have lastClassViewed column populated on creation now

--- server/seed/index.js
+++ server/seed/index.js
@@ -28,15 +28,15 @@ const seedFunction = () => {
   .then((user) => {
     user.addClasses([1, 2, 3, 4]);
   })
-  .then(() => models.user.create(userData[1]))
+  .then(() => models.user.create(Object.assign(userData[1], { lastClassViewed: enrollmentCode })))
   .then((user) => {
     user.addClasses([1, 2, 3, 4]);
   })
-  .then(() => models.user.create(userData[2]))
+  .then(() => models.user.create(Object.assign(userData[2], { lastClassViewed: enrollmentCode })))
   .then((user) => {
     user.addClasses([1, 2, 3]);
   })
-  .then(() => models.user.create(userData[3]))
+  .then(() => models.user.create(Object.assign(userData[3], { lastClassViewed: enrollmentCode })))
   .then((user) => {
     user.addClasses([1, 2, 3]);
   })
