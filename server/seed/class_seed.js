dea40f763db3fc673f534ebdb8ec9741e411d293
Gabemb <gabrielbny@gmail.com>
2017-02-24 18:00:39 -0500

    demo dummy data

--- server/seed/class_seed.js
+++ server/seed/class_seed.js
@@ -1,22 +1,22 @@
 module.exports = [
   {
-    name: 'Chemistry 123',
-    description: 'Chemistry is Sodium fun!',
+    name: 'Full Stack Web',
+    description: 'Producing developers who are comfortable working with both back-end and front-end technologies. A developer that can work with databases, PHP, HTML, CSS, JavaScript and everything in between. Even venturing as far as converting Photoshop designs to front-end code.',
     schedule: 'Tuesdays, 3:00pm \n Sunday, 8:00am \n Saturday, 8:00am',
     location: 'Room 110B',
   }, {
-    name: 'Biology',
-    description: 'I find this humerus.',
+    name: 'Android',
+    description: 'Teaching the Java programming language and how to use the Android software development kit (SDK)',
     schedule: 'Wednesdays, 3:00pm \n Friday, 10:00am \n Sunday, 6:30pm',
     location: 'Room 510',
   }, {
-    name: 'Physics',
-    description: 'Physics is phun!',
+    name: 'iOS',
+    description: 'Enabling people to create applications for Apple\'s iOS devices by teaching them the specialized Swift language.',
     schedule: 'Monday, 3:00pm \n Wednesdays, 12:00pm \n Friday, 9:45am',
     location: 'Room 510',
   }, {
-    name: 'Math',
-    description: 'Don\'t be late for taking the rhombus!',
+    name: 'Machine Learning',
+    description: 'Teaching how to create computers with the ability to learn without being explicitly programmed. Machine learning focuses on the development of computer programs that can change when exposed to new data. The process of machine learning is similar to that of data mining.',
     schedule: 'Thursdays, 3:00pm \n Wednesdays, 4:00pm \n Tuesdays, 2:00pm',
     location: 'Room 510',
   },
