2d6111d759a5e2257fce47b2aaa09a0770ed82dc
Samantha Bretous <sam@samanthabretous.com>
2017-02-25 22:26:15 -0500

    merge

---
+++ client/web_view/components/DisplayClasses/DisplayClassesStyles.js
@@ -0,0 +1,11 @@
+import styles from '../../containers/App/styles';
+
+const { colors } = styles;
+
+export default {
+  displayClasses: {
+    position: 'relative',
+    zIndex: 1000,
+    background: colors.grannyApple,
+  },
+};
